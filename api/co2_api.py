b666ad31234c723887e3e442ff71eefe4c76e857
ldover <luka.dover@gmail.com>
2018-06-02 14:23:48 +0200

    co2 visualization before and after industrial revolution. Fitted co2 graph with sine function

--- api/co2_api.py
+++ api/co2_api.py
@@ -1,9 +1,21 @@
 import pandas as pd
-from os.path import join, abspath, pardir
+from util import resource_path
+
 
 def get_data():
-    return pd.read_csv(abspath(join(pardir, "data", "co2.csv")), delimiter=";")
+    """
 
+    :return: DataFrame(kyear, co2, sigma_mean)
+    """
+    data = pd.read_csv(resource_path("data/co2.csv"), delimiter=";")
+    data.yearBP = data.yearBP * -1
+    data.yearBP = data.yearBP - data.yearBP.max()
+    data['kyear'] = data.yearBP / 1000
+    del data['yearBP']
+
+    data = data[data.kyear > - 380]
+
+    return data
 
 if __name__ == "__main__":
     data = get_data()
